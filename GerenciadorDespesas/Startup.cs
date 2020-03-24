de14e4b0b6e893dd95c440b6663da18ec277bafe
eversonsilva7 <eversonsilva@outlook.com>
2020-03-24 11:00:04 -0300

    primeiro commit

--- GerenciadorDespesas/Startup.cs
+++ GerenciadorDespesas/Startup.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GerenciadorDespesas.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -23,14 +26,8 @@ namespace GerenciadorDespesas
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
 
-
+            services.AddDbContext<Contexto>(opcoes => opcoes.UseSqlServer(Configuration.GetConnectionString("Conexao")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -44,16 +41,17 @@ namespace GerenciadorDespesas
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
             }
 
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();           
 
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Despesas}/{action=Index}/{id?}");
             });
         }
     }
---
+++ GerenciadorDespesas/ViewComponents/EstatisticasViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using GerenciadorDespesas.Models;
+using GerenciadorDespesas.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorDespesas.ViewComponents
+{
+    public class EstatisticasViewComponent : ViewComponent
+    {
+        private readonly Contexto _context;
+
+        public EstatisticasViewComponent(Contexto context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            EstatisticasViewModel estatisticas = new EstatisticasViewModel();
+
+            if (_context.Despesas.Count() > 0 && _context.Despesas != null)
+            {
+                estatisticas.MenorDespesa = _context.Despesas.Min(x => x.Valor);
+                estatisticas.MaiorDespesa = _context.Despesas.Max(x => x.Valor);
+                estatisticas.QuantidadeDespesas = _context.Despesas.Count();
+            }
+
+            return View(estatisticas);
+        }
+    }
+}
